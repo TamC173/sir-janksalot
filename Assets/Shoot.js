422a303ad46713273b4edba32ccefb042044e8db
daniel habetz <danielhabetz93@yahoo.com>
2015-03-23 22:06:47 +1100

    my changes

---
+++ Assets/Shoot.js
@@ -0,0 +1,20 @@
+﻿
+
+var projectile : Rigidbody;
+var speed = 10;
+
+
+
+function Update ()
+{
+
+	if(Input.GetButtonUp ("Fire1"))
+	{
+	clone = Instantiate(projectile, transform.position, transform.rotation);
+	clone.velocity = transform.TransformDirection(Vector3 (0, 0, speed));
+
+	Destroy (clone.gameObject, 2);
+	GetComponent.<ParticleSystem>().Play();
+	}
+	
+}
